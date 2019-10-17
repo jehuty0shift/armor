8cb7a1df32ce49215b97bb43c5e361c9b952d10d
Babacar Diassé <babacar.diasse@corp.ovh.com>
2019-10-17 14:49:35 +0200

    maintenance mode + GET _armor/ allowed form everywhere

--- src/main/java/com/petalmd/armor/rest/ArmorInfoAction.java
+++ src/main/java/com/petalmd/armor/rest/ArmorInfoAction.java
@@ -22,24 +22,30 @@ import com.petalmd.armor.service.ArmorConfigService;
 import com.petalmd.armor.util.ConfigConstants;
 import com.petalmd.armor.util.SecurityUtil;
 import org.apache.logging.log4j.Logger;
+import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.elasticsearch.common.bytes.BytesReference;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.rest.*;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
+import static org.elasticsearch.rest.RestRequest.Method.POST;
 
 public class ArmorInfoAction extends BaseRestHandler {
 
     private final Settings settings;
     private final ArmorConfigService armorConfigService;
     protected final Logger log = ESLoggerFactory.getLogger(ArmorInfoAction.class);
+    private static final AtomicBoolean available = new AtomicBoolean(true);
 
     @Inject
     public ArmorInfoAction(final Settings settings, RestController controller,
@@ -53,63 +59,111 @@ public class ArmorInfoAction extends BaseRestHandler {
     @Override
     public RestChannelConsumer prepareRequest(final RestRequest request, final NodeClient client) {
 
-        return new RestChannelConsumer() {
-
-            @Override
-            public void accept(RestChannel restChannel) throws Exception {
-
-
-                final boolean isLoopback = ((InetSocketAddress) request.getRemoteAddress()).getAddress().isLoopbackAddress();
-                final InetAddress resolvedAddress = SecurityUtil.getProxyResolvedHostAddressFromRequest(request, settings);
-
-                BytesRestResponse response;
-                final XContentBuilder builder = restChannel.newBuilder();
-
-                boolean available;
-                log.info("retrieving Security Configuration");
-                try {
-                    BytesReference securityConfiguration = armorConfigService.getSecurityConfiguration();
-                    if(securityConfiguration != null && securityConfiguration.length() > 0) {
-                        available = true;
-                    } else {
-                        available = false;
-                    }
-                } catch (Exception e) {
-                    available = false;
-                }
-
-                log.info("retrieved Security Configuration");
-                try {
-
-                    //TODO : To Delete ? Authentication is done in REST Wrapper
-
-                    builder.startObject();
-
-                    final Boolean enabled = settings.getAsBoolean(ConfigConstants.ARMOR_ENABLED,false);
-                    builder.field("enabled",enabled);
-                    builder.field("isloopback", isLoopback);
-                    builder.field("resolvedAddress", resolvedAddress.toString());
-                    builder.field("available", available);
-
-                    builder.endObject();
-                    if(log.isDebugEnabled()) {
-                        log.debug("enabled {}, isLoopback {}, resolvedAdress {}, available {}", enabled, isLoopback, resolvedAddress, available);
-                    }
-
-                    if (available) {
-                        response = new BytesRestResponse(RestStatus.OK, builder);
-                    } else {
-                        response = new BytesRestResponse(RestStatus.SERVICE_UNAVAILABLE, builder);
-                    }
-                } catch (final Exception e1) {
-                    builder.startObject();
-                    builder.field("error", e1.toString());
-                    builder.endObject();
-                    response = new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR, builder);
-                }
-                restChannel.sendResponse(response);
+        return restChannel -> {
+            if (request.method().equals(GET)) {
+                processGET(restChannel, request);
+            } else if (request.method().equals(POST)) {
+                processPOST(restChannel, request);
             }
+
         };
+
     }
+
+    public void processGET(RestChannel restChannel, RestRequest request) throws IOException {
+        final boolean isLoopback = ((InetSocketAddress) request.getRemoteAddress()).getAddress().isLoopbackAddress();
+        final InetAddress resolvedAddress = SecurityUtil.getProxyResolvedHostAddressFromRequest(request, settings);
+
+        BytesRestResponse response;
+        final XContentBuilder builder = restChannel.newBuilder();
+
+        boolean hasSecurityConf;
+        log.info("retrieving Security Configuration...");
+        try {
+            final BytesReference securityConfig = armorConfigService.getSecurityConfiguration();
+            hasSecurityConf = securityConfig != null && securityConfig.length() > 0;
+        } catch (ElasticsearchException e) {
+            hasSecurityConf = false;
+        }
+        log.info("retrieved Security Configuration.");
+
+        try {
+
+            builder.startObject();
+
+            final Boolean enabled = settings.getAsBoolean(ConfigConstants.ARMOR_ENABLED, false);
+            builder.field("enabled", enabled.toString());
+            builder.field("isLoopback", isLoopback);
+            builder.field("resolvedAddress", resolvedAddress.toString());
+            //availability depends on maintenance local mode
+            builder.field("available", available.get() ? hasSecurityConf : false);
+            log.debug("enabled {}, isLoopback {}, resolvedAddress {}, available {}", enabled, isLoopback, resolvedAddress, hasSecurityConf);
+            builder.endObject();
+            if (!available.get()) {
+                response = new BytesRestResponse(RestStatus.GONE, builder);
+            } else {
+                //should be available
+                if(hasSecurityConf) {
+                    response = new BytesRestResponse(RestStatus.OK, builder);
+                } else {
+                    response = new BytesRestResponse(RestStatus.SERVICE_UNAVAILABLE, builder);
+                }
+            }
+        } catch (final Exception e1) {
+            builder.startObject();
+            builder.field("error", e1.toString());
+            builder.endObject();
+            response = new BytesRestResponse(RestStatus.INTERNAL_SERVER_ERROR, builder);
+        }
+        log.info("Armor available: {}", hasSecurityConf);
+        restChannel.sendResponse(response);
+
+    }
+
+
+    public void processPOST(RestChannel restChannel, RestRequest restRequest) throws IOException {
+
+        BytesRestResponse response;
+        final XContentBuilder builder = restChannel.newBuilder();
+
+        if(!restRequest.hasContent()) {
+            builder.startObject();
+            builder.field("error","maintenance call needs a payload");
+            builder.endObject();
+
+            response = new BytesRestResponse(RestStatus.BAD_REQUEST, builder);
+            restChannel.sendResponse(response);
+            return;
+        }
+
+        XContentParser contentParser = restRequest.contentParser();
+        if(XContentParser.Token.START_OBJECT == contentParser.nextToken()) {
+            while (XContentParser.Token.END_OBJECT != contentParser.nextToken()) {
+                if(XContentParser.Token.FIELD_NAME == contentParser.currentToken()) {
+                    String currentFieldName = contentParser.currentName();
+                    if ("maintenance_enabled".equals(currentFieldName)) {
+                        if (XContentParser.Token.VALUE_BOOLEAN == contentParser.nextToken()) {
+                            available.set(!contentParser.booleanValue());
+                            builder.startObject();
+                            builder.field("acknowledged", true);
+                            builder.endObject();
+                            response = new BytesRestResponse(RestStatus.OK, builder);
+                            restChannel.sendResponse(response);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        builder.startObject();
+        builder.field("error","the content is not right (expected field 'maintenance_enabled'");
+        builder.endObject();
+        response = new BytesRestResponse(RestStatus.BAD_REQUEST, builder);
+
+        restChannel.sendResponse(response);
+
+    }
+
 
 }
